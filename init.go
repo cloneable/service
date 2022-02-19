3c893dd10331ddcb09d594c8287970addfe427e3
Folke Behrens <folke@gmail.com>
2022-02-19 19:27:25 +0100

    Add apply loops for options to Init and Run

--- init.go
+++ init.go
@@ -3,9 +3,17 @@ package service
 import "context"
 
 func Init(ctx context.Context, options ...InitOption) (context.Context, error) {
+	var cfg InitConfig
+	for _, option := range options {
+		if option != nil {
+			if err := option(ctx, &cfg); err != nil {
+				return ctx, err
+			}
+		}
+	}
 	return ctx, nil
 }
 
-type InitOption func(cfg *InitConfig) error
+type InitOption func(ctx context.Context, cfg *InitConfig) error
 
 type InitConfig struct{}
--- run.go
+++ run.go
@@ -3,9 +3,17 @@ package service
 import "context"
 
 func Run(ctx context.Context, options ...RunOption) error {
+	var cfg RunConfig
+	for _, option := range options {
+		if option != nil {
+			if err := option(ctx, &cfg); err != nil {
+				return err
+			}
+		}
+	}
 	return nil
 }
 
-type RunOption func(cfg *RunConfig) error
+type RunOption func(ctx context.Context, cfg *RunConfig) error
 
 type RunConfig struct{}
